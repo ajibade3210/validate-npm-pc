4bde7a0b0f52303c18b3050a2d2edd8575c68f94
olaoluwa ajibade <ajibade69th@gmail.com>
2024-05-19 18:50:26 +0100

    feature/test-changeset: testing

--- src/index.ts
+++ src/index.ts
@@ -40,6 +40,7 @@ export const validSocial = (url: string): boolean => {
   const socialRegexMap: Map<string, RegExp> = new Map([
     ["facebook", /^(https?:\/\/)?(www\.)?facebook.com\/[a-zA-Z0-9._-]+\/?$/],
     ["twitter", /^(https?:\/\/)?(www\.)?twitter.com\/[a-zA-Z0-9_]+\/?$/],
+    ["twitter", /^(https?:\/\/)?(www\.)?tiktok.com\/[a-zA-Z0-9_]+\/?$/],
     // Add more social platforms' regex patterns here
   ]);
   return Array.from(socialRegexMap.values()).some(regex => regex.test(url));
